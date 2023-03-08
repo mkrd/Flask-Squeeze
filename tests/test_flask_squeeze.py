193fdb8ecba7234fc37ce5078dafbc0bbf059782
Marcel Kröker <kroeker.marcel@gmail.com>
2023-03-09 00:12:48 +0100

    add tests

--- tests/test_flask_squeeze.py
+++ tests/test_flask_squeeze.py
@@ -37,3 +37,33 @@ def test_get_accept_no_encoding(client: FlaskClient):
     r = client.get('/', headers={})
     assert "Content-Encoding" not in r.headers
     assert r.headers["Content-Length"] == "3955741"
+
+
+def test_get_brotli_css_file(client: FlaskClient):
+    print("Using brotli to get css file:")
+    r = client.get('/static/main.css', headers={'Accept-Encoding': 'br;'})
+    assert r.headers["Content-Encoding"] == "br"
+
+
+def test_get_brotli_js_file(client: FlaskClient):
+    print("Using brotli to get css file:")
+    r = client.get('/static/main.js', headers={'Accept-Encoding': 'br;'})
+    assert r.headers["Content-Encoding"] == "br"
+    print(r.headers["X-Uncompressed-Content-Length"])
+    print(r.headers["Content-Length"])
+
+
+
+def test_get_gzip_css_file(client: FlaskClient):
+    print("Using gzip to get css file:")
+    r = client.get('/static/main.css', headers={'Accept-Encoding': 'gzip;'})
+    assert r.headers["Content-Encoding"] == "gzip"
+
+
+
+def test_get_gzip_js_file(client: FlaskClient):
+    print("Using gzip to get css file:")
+    r = client.get('/static/main.js', headers={'Accept-Encoding': 'gzip;'})
+    assert r.headers["Content-Encoding"] == "gzip"
+    print(r.headers["X-Uncompressed-Content-Length"])
+    print(r.headers["Content-Length"])
