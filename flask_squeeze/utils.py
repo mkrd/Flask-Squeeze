ca243b38f0dc9a6f98c31e129e93afc876c4912f
Marcel Kröker <kroeker.marcel@gmail.com>
2023-03-12 16:35:36 +0100

    code style

--- flask_squeeze/utils.py
+++ flask_squeeze/utils.py
@@ -1,7 +1,7 @@
 from enum import Enum
+from typing import Union
 
 from flask import Request, Response, current_app
-
 
 
 class Encoding(Enum):
@@ -18,7 +18,7 @@ class Minifcation(Enum):
 
 
 
-def choose_encoding(request: Request):
+def choose_encoding(request: Request) -> Union[Encoding, None]:
 	if not current_app.config["COMPRESS_FLAG"]:
 		return None
 	encoding = request.headers.get("Accept-Encoding", "").lower()
@@ -32,7 +32,7 @@ def choose_encoding(request: Request):
 
 
 
-def choose_minification(response: Response):
+def choose_minification(response: Response) -> Union[Minifcation, None]:
 	mimetype = response.mimetype
 	if (mimetype.endswith("javascript") or mimetype.endswith("json")) and current_app.config["COMPRESS_MINIFY_JS"]:
 		return Minifcation.js
