5631b0d55224d40f066f342bb7d2c5f8fa95e247
Marcel Kröker <marcel.kroeker@noimos.ai>
2025-01-23 10:12:40 +0100

    add test

--- tests/test_flask_squeeze.py
+++ tests/test_flask_squeeze.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 import pytest
 from flask.testing import FlaskClient
 from test_app import create_app
@@ -50,7 +53,7 @@ def content_length_correct(r: Response) -> bool:
 ########
 
 
-def test_get_index(client: FlaskClient, use_encoding: str):
+def test_get_index(client: FlaskClient, use_encoding: str) -> None:
 	print("test_get_index")
 	r = client.get("/", headers={"Accept-Encoding": use_encoding})
 	assert content_length_correct(r)
@@ -69,7 +72,7 @@ def test_get_index(client: FlaskClient, use_encoding: str):
 	assert almost_equal(length, sizes[use_encoding])
 
 
-def test_get_css_file(client: FlaskClient, use_encoding: str, use_minify_css: bool):
+def test_get_css_file(client: FlaskClient, use_encoding: str, use_minify_css: bool) -> None:
 	print("test_get_css_file with", use_encoding, "minify:", use_minify_css)
 	client.application.config.update({"SQUEEZE_MINIFY_CSS": use_minify_css})
 	r = client.get("/static/fomantic.css", headers={"Accept-Encoding": use_encoding})
@@ -93,7 +96,7 @@ def test_get_css_file(client: FlaskClient, use_encoding: str, use_minify_css: bo
 	assert almost_equal(response_length, sizes[(use_encoding, use_minify_css)])
 
 
-def test_get_js_file(client: FlaskClient, use_encoding: str, use_minify_js: bool):
+def test_get_js_file(client: FlaskClient, use_encoding: str, use_minify_js: bool) -> None:
 	print("test_get_js_file with", use_encoding, "minify:", use_minify_js)
 	client.application.config.update({"SQUEEZE_MINIFY_JS": use_minify_js})
 	r = client.get("/static/jquery.js", headers={"Accept-Encoding": use_encoding})
@@ -101,7 +104,7 @@ def test_get_js_file(client: FlaskClient, use_encoding: str, use_minify_js: bool
 	assert use_encoding == r.headers.get("Content-Encoding", "")
 
 
-def test_get_jquery_no_minify(client: FlaskClient):
+def test_get_jquery_no_minify(client: FlaskClient) -> None:
 	client.application.config.update({"SQUEEZE_MINIFY_JS": False})
 	r_orig = client.get("/static/jquery.js", headers={})
 	assert content_length_correct(r_orig)
@@ -110,19 +113,19 @@ def test_get_jquery_no_minify(client: FlaskClient):
 	assert "X-Uncompressed-Content-Length" not in r_orig.headers
 
 
-def test_get_from_cache(client: FlaskClient, use_encoding: str):
+def test_get_from_cache(client: FlaskClient, use_encoding: str) -> None:
 	client.application.config.update({"SQUEEZE_MINIFY_JS": False})
 	r = client.get("/static/jquery.min.js", headers={"Accept-Encoding": use_encoding})
 	r_2 = client.get("/static/jquery.min.js", headers={"Accept-Encoding": use_encoding})
 	assert r.data == r_2.data
 
 
-def test_get_unknown_url(client: FlaskClient):
+def test_get_unknown_url(client: FlaskClient) -> None:
 	r = client.get("/static/unknown.js", headers={"Accept-Encoding": "gzip"})
 	assert r.status_code == 404
 
 
-def test_get_same_repeatedly(client: FlaskClient):
+def test_get_same_repeatedly(client: FlaskClient) -> None:
 	client.application.config.update({"SQUEEZE_MINIFY_JS": True})
 	for i in range(100):
 		r = client.get("/static/jquery.js", headers={"Accept-Encoding": "br"})
@@ -153,7 +156,7 @@ def response_has_vary_header(r: Response) -> bool:
 ########################################################################################
 
 
-def test_disable_compression(client: FlaskClient):
+def test_disable_compression(client: FlaskClient) -> None:
 	"""Test that compression can be disabled."""
 	client.application.config.update({"SQUEEZE_COMPRESS": False})
 	r = client.get("/static/jquery.js", headers={"Accept-Encoding": "gzip"})
@@ -161,7 +164,7 @@ def test_disable_compression(client: FlaskClient):
 	assert not response_has_breach_header(r)
 
 
-def test_disable_minification(client: FlaskClient):
+def test_disable_minification(client: FlaskClient) -> None:
 	"""Test that minification can be disabled."""
 	client.application.config.update(
 		{"SQUEEZE_MINIFY_JS": False, "SQUEEZE_MINIFY_CSS": False, "SQUEEZE_MINIFY_HTML": False}
@@ -170,26 +173,26 @@ def test_disable_minification(client: FlaskClient):
 	assert "X-Flask-Squeeze-Minify-Duration" not in r.headers
 
 
-def test_html_response_minification(client: FlaskClient):
+def test_html_response_minification(client: FlaskClient) -> None:
 	"""Test minification of HTML responses."""
 	client.application.config.update({"SQUEEZE_MINIFY_HTML": True})
 	r = client.get("/", headers={"Accept-Encoding": "gzip"})
 	assert "X-Flask-Squeeze-Minify-Duration" in r.headers
 
 
-def test_breach_header_presence(client: FlaskClient):
+def test_breach_header_presence(client: FlaskClient) -> None:
 	"""Test the presence of the breach exploit protection header."""
 	r = client.get("/", headers={"Accept-Encoding": "br"})
 	assert response_has_breach_header(r)
 
 
-def test_vary_header_presence(client: FlaskClient):
+def test_vary_header_presence(client: FlaskClient) -> None:
 	"""Test the presence of the Vary header after compression."""
 	r = client.get("/static/jquery.js", headers={"Accept-Encoding": "gzip"})
 	assert response_has_vary_header(r)
 
 
-def test_minification_and_compression_together(client: FlaskClient):
+def test_minification_and_compression_together(client: FlaskClient) -> None:
 	"""Test both minification and compression are applied together."""
 	client.application.config.update({"SQUEEZE_MINIFY_CSS": True})
 	r = client.get("/static/fomantic.css", headers={"Accept-Encoding": "gzip"})
@@ -198,9 +201,43 @@ def test_minification_and_compression_together(client: FlaskClient):
 	assert content_length_correct(r)
 
 
-def test_debug_headers_presence(client: FlaskClient):
+def test_debug_headers_presence(client: FlaskClient) -> None:
 	"""Test if debug headers are added when enabled."""
 	client.application.config.update({"SQUEEZE_ADD_DEBUG_HEADERS": True})
 	r = client.get("/static/fomantic.css", headers={"Accept-Encoding": "gzip"})
 	assert "X-Flask-Squeeze-Total-Duration" in r.headers
 	assert "X-Flask-Squeeze-Compress-Duration" in r.headers or "X-Flask-Squeeze-Minify-Duration" in r.headers
+
+
+def test_static_file_cache_behavior(client: FlaskClient) -> None:
+	"""Test cache behavior for a temporary static file."""
+
+	# Ensure the static directory is set
+	if (static_dir := client.application.static_folder) is None:
+		raise ValueError("Static directory not found")
+
+	# Use a temporary file within the static directory
+	with tempfile.NamedTemporaryFile(dir=static_dir, suffix=".txt", delete=True) as temp_file:
+		file_path = temp_file.name
+		temp_file.write(b"Initial content")
+		temp_file.flush()  # Ensure content is written to disk
+
+		# First request: MISS
+		r = client.get(f"/static/{os.path.basename(file_path)}", headers={"Accept-Encoding": "gzip"})
+		assert r.headers.get("X-Flask-Squeeze-Cache") == "MISS"
+
+		# Second request: HIT
+		r = client.get(f"/static/{os.path.basename(file_path)}", headers={"Accept-Encoding": "gzip"})
+		assert r.headers.get("X-Flask-Squeeze-Cache") == "HIT"
+
+		# Modify the file content
+		temp_file.write(b"Updated content")
+		temp_file.flush()  # Ensure the new content is written
+
+		# Third request: MISS
+		r = client.get(f"/static/{os.path.basename(file_path)}", headers={"Accept-Encoding": "gzip"})
+		assert r.headers.get("X-Flask-Squeeze-Cache") == "MISS"
+
+		# Fourth request: HIT
+		r = client.get(f"/static/{os.path.basename(file_path)}", headers={"Accept-Encoding": "gzip"})
+		assert r.headers.get("X-Flask-Squeeze-Cache") == "HIT"
