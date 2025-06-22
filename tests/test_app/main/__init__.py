c7d8ab96e39d26f6cbb4c198d9d1ae32943c8689
Marcel Kröker <kroeker.marcel@gmail.com>
2025-06-22 12:34:35 +0200

    fix noqa

--- tests/test_app/main/__init__.py
+++ tests/test_app/main/__init__.py
@@ -1,5 +1,4 @@
-# flake8: noqa
 from flask import Blueprint
 
 bp = Blueprint("main", __name__, template_folder="templates")
-from test_app.main import routes
+from test_app.main import routes  # noqa: E402, F401
